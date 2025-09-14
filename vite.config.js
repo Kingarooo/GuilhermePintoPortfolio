5b8ac29d2e0a85ec5b95b520e609cd35aab60c62
Guilherme Pinto <guilhermepinto@safeguardworld.com>
2025-09-14 01:15:45 +0100

    docs for github

--- vite.config.js
+++ vite.config.js
@@ -14,4 +14,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  // Output to `docs/` so GitHub Pages can serve the built site directly from the repo
+  build: {
+    outDir: 'docs'
+  }
 })
